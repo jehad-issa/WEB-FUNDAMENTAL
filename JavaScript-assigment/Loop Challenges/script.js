a0bad93eda4612f3b6a952a36ca3a9baf9fad463
jehad-issa <96985283+jehad-issa@users.noreply.github.com>
2022-01-15 15:40:43 +0200

    loop challenges

---
+++ JavaScript-assigment/Loop Challenges/script.js
@@ -0,0 +1,33 @@
+// print odd num from 1-20 //
+for(i=1; i<=20; i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+//Decreasing multiples of 3 from 100-0 //
+
+for(i=100; i>=0; i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+
+// print the sequence //
+for(i=4; i>=-3.5; i-=1.5){
+    console.log(i);
+    }
+
+    // sigma all sum num from 1-100 //
+
+    var sum=0;
+for(i=1; i<=100; i++){
+    console.log(sum+=i);
+}
+
+//Factorial: multibly all of the value from 1-12 //
+var product=1;
+for(i=1; i<=12; i++){
+    console.log(product*=i);
+}
